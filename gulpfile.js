f0374bb8a821a9082b8dfa9278d6fcb9b3af89b7
Manjunath <manjumjng2@gmail.com>
2024-09-10 14:14:41 +0530

    Fix devImages being declared twice

--- gulpfile.js
+++ gulpfile.js
@@ -79,12 +79,6 @@ function devImages() {
   );
 }
 
-function devImages() {
-  return src(`${options.paths.src.img}/**/*`).pipe(
-    dest(options.paths.dist.img)
-  );
-}
-
 function devFonts() {
   return src(`${options.paths.src.fonts}/**/*`).pipe(
     dest(options.paths.dist.fonts)
